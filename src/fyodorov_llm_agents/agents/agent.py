9a2fcacdaf957af8dae5fe7ad8d936dc82a123f5
Daniel Ransom <dantheman0207@users.noreply.github.com>
2024-03-07 17:46:48 +0100

    check if key exists

--- src/fyodorov_llm_agents/agents/agent.py
+++ src/fyodorov_llm_agents/agents/agent.py
@@ -94,7 +94,7 @@ class Agent(BaseModel):
             print(f"calling litellm with model {self.model}, messages: {messages}, max_retries: 0, history: {history}")
             response = litellm.completion(model=model, messages=messages, max_retries=0)
         print(f"Response: {response}")
-        tool_calls = response.tool_calls
+        tool_calls = response.tool_calls if response.tool_calls else []
         if tool_calls:
             for tool_call in tool_calls:
                 print(f"Calling function {tool_call.function.name}")
